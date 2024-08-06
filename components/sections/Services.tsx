8eb0bb12ba330651645489cb7d73936f24e35f52
Chamika Madhushan <chamikamadhushan35@gmail.com>
2024-08-07 00:30:53 +0530

    updated services

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -1,14 +1,55 @@
-import Image from 'next/image';
 import React from 'react';
 import ServiceCard from '../ServiceCard';
-import { FaMobile } from "react-icons/fa";
-import { TbWorld } from "react-icons/tb";
-import { IoBarChart } from "react-icons/io5";
-import { RiShoppingCart2Fill } from "react-icons/ri";
-import { AiOutlineCloudServer } from "react-icons/ai";
-import { FiMonitor } from "react-icons/fi";
-import {  FaCogs } from "react-icons/fa";
-import { MdOutlineContentCopy } from "react-icons/md";
+import { FaMobile, FaCogs } from 'react-icons/fa';
+import { IoBarChart } from 'react-icons/io5';
+import { TbWorld } from 'react-icons/tb';
+import { RiShoppingCart2Fill } from 'react-icons/ri';
+import { AiOutlineCloudServer } from 'react-icons/ai';
+import { FiMonitor } from 'react-icons/fi';
+import { MdOutlineContentCopy } from 'react-icons/md';
+
+const serviceData = [
+    {
+        title: 'Web Application Development',
+        description: 'Build powerful and scalable web applications tailored to your business needs.',
+        icon: <TbWorld />,
+    },
+    {
+        title: 'Mobile App Development',
+        description: 'Develop engaging mobile applications to reach your customers on their favorite devices.',
+        icon: <FaMobile />,
+    },
+    {
+        title: 'E-commerce Solutions',
+        description: 'Launch a successful online store with our comprehensive e-commerce solutions.',
+        icon: <RiShoppingCart2Fill />,
+    },
+    {
+        title: 'Cloud Computing',
+        description: 'Leverage cloud technology for efficient and scalable business operations.',
+        icon: <AiOutlineCloudServer />,
+    },
+    {
+        title: 'UI/UX Design',
+        description: 'Design engaging and user-friendly interfaces for web and mobile applications.',
+        icon: <FiMonitor />,
+    },
+    {
+        title: 'Content Management Systems',
+        description: 'Manage your digiatl content easily with our customizable CMS solutions.',
+        icon: <MdOutlineContentCopy />,
+    },
+    {
+        title: 'Search Engine Optimization',
+        description: 'Improve your website\'s visibility and ranking on search engines.',
+        icon: <IoBarChart />,
+    },
+    {
+        title: 'IT Consulting',
+        description: 'Get expert advice and strategies to optimize your IT infrastructure and operations.',
+        icon: <FaCogs />,
+    },
+];
 
 const Services = () => {
     return (
@@ -19,15 +60,14 @@ const Services = () => {
                     <p className="text-gray-500 sm:text-xl text-center dark:text-gray-400">At MSIC Labs, we deliver innovative solutions to help you thrive in a digital world.</p>
                 </div>
                 <div className="grid gap-8 justify-items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                    <ServiceCard title="Mobile App Development" description="Create intuitive and high-performing mobile applications tailored to your needs." icon={<FaMobile />} />
-                    <ServiceCard title="Web Development" description="Build responsive and scalable web solutions that drive business growth." icon={<TbWorld />} />
-                    <ServiceCard title="E-commerce Solutions" description="Develop comprehensive e-commerce platforms to enhance your online sales." icon={<RiShoppingCart2Fill />} />
-                    {/* <ServiceCard title="Digital Marketing" description="Boost your online presence with targeted digital marketing strategies." icon={<IoBarChart />} /> */}
-                    <ServiceCard title="Cloud Computing" description="Leverage cloud technology for efficient and scalable business operations." icon={<AiOutlineCloudServer />} />
-                    <ServiceCard title="UI/UX Design" description="Design engaging and user-friendly interfaces for web and mobile applications." icon={<FiMonitor />} />
-                    <ServiceCard title="Content Management Systems" description="Develop and manage your content easily with our customizable CMS solutions." icon={<MdOutlineContentCopy />} />
-                    <ServiceCard title="Search Engine Optimization" description="Improve your website's visibility and ranking on search engines." icon={<IoBarChart />} />
-                    <ServiceCard title="IT Consulting" description="Get expert advice and strategies to optimize your IT infrastructure and operations." icon={<FaCogs />} />
+                    {serviceData.map((service, index) => (
+                        <ServiceCard
+                            key={index}
+                            title={service.title}
+                            description={service.description}
+                            icon={service.icon}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
