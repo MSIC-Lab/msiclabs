89bac1379b4fb60ffd1dc0ede8f4add440fbb9f1
milindaShehan <milinda.msd@gmail.com>
2024-08-06 11:22:07 +0530

    ref

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
-import Image from "next/image";
-import Hero from "@/components/sections/hero";
+// import Image from "next/image";
+import Hero from "@/components/sections/Hero";
 import Navbar from "@/components/ui/navbar";
-import Services from "@/components/sections/services";
-import Technologies from "@/components/sections/technologies";
-import Contact from "@/components/sections/contact";
+import Customer from "@/components/sections/Customers";
+// import Services from "@/components/sections/services";
+// import Technologies from "@/components/sections/technologies";
+// import Contact from "@/components/sections/contact";
 
 
 export default function Home() {
   return (
     <>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <Services></Services>
-      <Technologies></Technologies>
-      <Contact></Contact>
+      <Navbar />
+      <Hero />
+      <Customer />
+      
+     
     </>
   );
 }
---
+++ components/sections_old/hero.tsx
@@ -0,0 +1,42 @@
+
+
+import React from "react";
+import Fadein from "../transitions/fadein";
+import Rollup from "../transitions/rollup";
+
+
+const hero = () => {
+  return (
+    <>
+      <main className="w-screen px-20 h-[calc(100vh-4rem)] bg-gradient-to-r from-violet-50  via-purple-100 to-fuchsia-50 flex justify-center items-start bg-fixed ">
+        <div className="max-w-[1200px] w-[95%] sm:w-full  flex justify-center items-center text-center flex-col">
+          <div
+            className="py-36 sm:px-16 text-blue-600 "
+          >
+            <Fadein
+              text="  Crafting Tommorrow's Tech,"
+              className={"text-5xl md:text-7xl font-medium text-primary-1 "}
+            ></Fadein>
+            <Fadein
+              delay={2.4}
+              text="Today !"
+              className={"text-5xl md:text-7xl font-medium  text-primary-1"}
+            ></Fadein>
+          </div>
+
+          <Rollup
+            text={
+              "   Join us in revolutionizing the digital landscape and shaping the future together !"
+            }
+            className={"text-2xl font-thin"}
+          ></Rollup>
+        </div>
+        <div>
+          <h1 className="text-[20rem]">M</h1>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default hero;
