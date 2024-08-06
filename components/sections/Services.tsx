20f0f3b9eb57ca588cb896c0d5381644e7cf51bb
Chamika Madhushan <chamikamadhushan35@gmail.com>
2024-08-06 23:36:07 +0530

    added service contents

--- components/sections/Services.tsx
+++ components/sections/Services.tsx
@@ -1,10 +1,20 @@
-import Image from 'next/image';
 import React from 'react';
 import ServiceCard from '../ServiceCard';
-import { FaMobile } from "react-icons/fa";
+import { FaMobile, FaShoppingCart, FaWordpress, FaBusinessTime } from "react-icons/fa";
 import { TbWorld } from "react-icons/tb";
-import { IoBarChart } from "react-icons/io5";
-import { RiShoppingCart2Fill } from "react-icons/ri";
+import { IoSpeedometerOutline } from "react-icons/io5";
+import { RiCodeSSlashLine } from "react-icons/ri";
+
+const servicesData = [
+    { title: "Web Application Development", description: "Build powerful and scalable web applications tailored to your business needs.", icon: <RiCodeSSlashLine /> },
+    { title: "Business/Personal Website Development", description: "Create a strong online presence with a professional website.", icon: <TbWorld /> },
+    { title: "Mobile App Development", description: "Develop engaging mobile applications to reach your customers on their favorite devices.", icon: <FaMobile /> },
+    { title: "Performance Optimization", description: "Improve the performance and speed of your website or application for a seamless user experience.", icon: <IoSpeedometerOutline /> },
+    { title: "E-Commerce Solutions", description: "Launch a successful online store with our comprehensive e-commerce solutions.", icon: <FaShoppingCart /> },
+    { title: "Customized POS Solutions", description: "Implement tailored point-of-sale systems to streamline your business operations and enhance customer service.", icon: <FaBusinessTime /> },
+    { title: "Content Management Systems", description: "Easily manage your digital content with our user-friendly CMS solutions.", icon: <TbWorld /> },
+    { title: "WordPress Development", description: "Harness the power of WordPress to create a versatile and customizable website.", icon: <FaWordpress /> }
+];
 
 const Services = () => {
     return (
@@ -15,14 +25,9 @@ const Services = () => {
                     <p className="text-gray-500 sm:text-xl text-center dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                 </div>
                 <div className="grid gap-8 justify-items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                    <ServiceCard title="Mobile App Development" description="Get a better understanding of where your traffic is coming from." icon={<FaMobile />} />
-                    <ServiceCard title="Web Development" description="Speak directly to your customers in a more meaningful way." icon={<TbWorld />} />
-                    <ServiceCard title="E-commerce" description="Optimize your website for search engines and get more traffic." icon={<RiShoppingCart2Fill />} />
-                    <ServiceCard title="Digital Marketing" description="Get a better understanding of where your traffic is coming from." icon={<IoBarChart />} />
-                    <ServiceCard title="Mobile App Development" description="Get a better understanding of where your traffic is coming from." icon={<FaMobile />} />
-                    <ServiceCard title="Web Development" description="Speak directly to your customers in a more meaningful way." icon={<TbWorld />} />
-                    <ServiceCard title="E-commerce" description="Optimize your website for search engines and get more traffic." icon={<FaMobile />} />
-                    <ServiceCard title="Digital Marketing" description="Get a better understanding of where your traffic is coming from." icon={<IoBarChart />} />
+                    {servicesData.map((service, index) => (
+                        <ServiceCard key={index} title={service.title} description={service.description} icon={service.icon} />
+                    ))}
                 </div>
             </div>
         </section>
