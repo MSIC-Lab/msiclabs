324c001a7277287ab2a944423f6c57ddacd733d0
milindaShehan <milinda.msd@gmail.com>
2024-08-06 11:22:17 +0530

    Hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,40 +1,50 @@
+import Image from 'next/image';
+import React from 'react';
 
-
-import React from "react";
-import Fadein from "../transitions/fadein";
-import Rollup from "../transitions/rollup";
-
-
-const hero = () => {
-  return (
-    <>
-      <main className="w-screen h-[calc(100vh-4rem)] bg-gradient-to-r from-violet-50  via-purple-100 to-fuchsia-50 flex justify-center items-start bg-fixed">
-        <div className="max-w-[1200px] w-[95%] sm:w-full  flex justify-center items-center text-center flex-col">
-          <div
-        
-            className="py-36 sm:px-16"
-          >
-            <Fadein
-              text="  Crafting Tommorrow's Tech,"
-              className={"text-5xl md:text-7xl font-medium "}
-            ></Fadein>
-            <Fadein
-              delay={2.4}
-              text="Today !"
-              className={"text-5xl md:text-7xl font-medium text-primary-1"}
-            ></Fadein>
-          </div>
-
-          <Rollup
-            text={
-              "   Join us in revolutionizing the digital landscape and shaping the future together !"
-            }
-            className={"text-2xl font-thin"}
-          ></Rollup>
+const Hero = () => {
+    return (
+        <div className="hero">
+            <section className="bg-white dark:bg-gray-900">
+                <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+                    <div className="mr-auto place-self-center lg:col-span-7">
+                        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                            Payments tool for software companies
+                        </h1>
+                        <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                            From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
+                        </p>
+                        <a
+                            href="#"
+                            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                        >
+                            Get started
+                            <svg
+                                className="w-5 h-5 ml-2 -mr-1"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        </a>
+                        <a
+                            href="#"
+                            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                        >
+                            Speak to Sales
+                        </a>
+                    </div>
+                    <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+                        <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup" width={500} height={500} />
+                    </div>
+                </div>
+            </section>
         </div>
-      </main>
-    </>
-  );
+    );
 };
 
-export default hero;
+export default Hero;
