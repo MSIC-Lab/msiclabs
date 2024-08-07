8729462f389ed1a7b2e4249a22da9fd0ec9bdd84
milindaShehan <milinda.msd@gmail.com>
2024-08-07 14:40:56 +0530

    hero

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -8,8 +8,11 @@ const Hero = () => {
       <section className="bg-white dark:bg-gray-900">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-xl xl:text-2xl text-primary-1 dark:text-white">
+            Crafting Tommorrow's Tech, Today! 
+            </h1>
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-black dark:text-white">
-              Payments tool for software companies
+              Payment tool for 
             </h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               From checkout to global sales tax compliance, companies around the
@@ -46,7 +49,7 @@ const Hero = () => {
                 </div>
                 <div className="flex flex-col items-center justify-center">
                   <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-                    <NumberTicker value={30} />+
+                    <NumberTicker value={20} />+
                   </dt>
                   <dd className="font-light text-gray-500 dark:text-gray-400">
                     Clients
