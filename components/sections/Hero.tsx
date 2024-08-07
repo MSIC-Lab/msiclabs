586a15a537104b889a23f2878bc2553d557a704c
Sonal Induwara <sonalinduwara@gmail.com>
2024-08-07 18:55:21 +0530

    hero

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import NumberTicker from "@/components/magicui/number-ticker";
+import Rollup from "../transitions/rollup";
+import Fadein from "../transitions/fadein";
 
 const Hero = () => {
   return (
@@ -8,9 +10,14 @@ const Hero = () => {
       <section className="bg-white dark:bg-gray-900">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-xl xl:text-2xl text-primary-1 dark:text-white">
+          {/* <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-xl xl:text-2xl text-primary-1 dark:text-white">
             Crafting Tommorrow's Tech, Today! 
-            </h1>
+            </h1> */}
+
+            <Fadein
+              text="  Crafting Tommorrow's Tech, Today!"
+              className={"max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-xl xl:text-2xl text-primary-1 dark:text-white "}
+            ></Fadein>
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-black dark:text-white">
               Payment tool for 
             </h1>
@@ -38,7 +45,7 @@ const Hero = () => {
             </a>
 
             <div className="mt-10 bg-gray-100 p-4 rounded-2xl">
-              <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
+              <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 grid-cols-3 dark:text-white">
                 <div className="flex flex-col items-center justify-center">
                   <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
                   <NumberTicker value={20} />+
