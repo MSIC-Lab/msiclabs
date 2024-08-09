ec171a19b05a5b0dce670914cc818afc3cf01296
Chamika Madhushan <chamikamadhushan35@gmail.com>
2024-08-09 11:48:57 +0530

    contact content fixed

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -56,7 +56,7 @@ export default function Contact() {
             id="email"
             name="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="name@flowbite.com"
+            placeholder="name@mail.com"
             required
           />
         </div>
@@ -87,7 +87,7 @@ export default function Contact() {
             id="message"
             name="message"
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Leave a comment..."
+            placeholder="Leave a message..."
             required
           ></textarea>
         </div>
